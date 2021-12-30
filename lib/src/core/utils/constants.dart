0a589ccfaf4a5e78d289ad31e4726c3689b7b25b
EhsanTgv <EhsanTgv@gmail.com>
2021-12-30 14:06:13 +0300

    Implement folder structure

---
+++ lib/src/core/utils/constants.dart
@@ -0,0 +1,11 @@
+const String kMaterialAppTitle = 'Flutter Clean Architecture';
+
+// API
+const String kBaseUrl = 'https://newsapi.org/v2';
+
+const String kApiKey = 'e9e8bfa4d0784af0a5dbe4573046fb51';
+
+// Database
+const String kArticlesTableName = 'articles_table';
+
+const String kDatabaseName = 'app_database.db';
