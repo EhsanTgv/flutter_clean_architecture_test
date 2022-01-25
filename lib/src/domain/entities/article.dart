8bb7f8d4a65e240dd74945c773c865f59fcc5a4a
EhsanTgv <EhsanTgv@gmail.com>
2022-01-25 22:27:14 +0300

    Implement entities

---
+++ lib/src/domain/entities/article.dart
@@ -0,0 +1,43 @@
+import 'package:equatable/equatable.dart';
+import 'package:flutter_clean_architecture_test/src/domain/entities/source.dart';
+
+class Article extends Equatable {
+  final int id;
+
+  final Source source;
+  final String author;
+  final String title;
+  final String description;
+  final String url;
+  final String urlToImage;
+  final String publishedAt;
+  final String content;
+
+  Article({
+    required this.id,
+    required this.source,
+    required this.author,
+    required this.title,
+    required this.description,
+    required this.url,
+    required this.urlToImage,
+    required this.publishedAt,
+    required this.content,
+  });
+
+  @override
+  List<Object> get props => [
+        id,
+        source,
+        author,
+        title,
+        description,
+        url,
+        urlToImage,
+        publishedAt,
+        content,
+      ];
+
+  @override
+  bool get stringify => true;
+}
