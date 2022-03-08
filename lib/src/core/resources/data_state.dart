1d79d170b7986600192ac262da2cd8bf1463189a
EhsanTgv <EhsanTgv@gmail.com>
2022-03-08 12:40:37 +0300

    Implement Models

--- lib/src/core/resources/data_state.dart
+++ lib/src/core/resources/data_state.dart
@@ -1,16 +1,16 @@
 import 'package:dio/dio.dart';
 
 abstract class DataState<T> {
-  final T data;
-  final DioError error;
+  final T? data;
+  final DioError? error;
 
-  const DataState(this.data, this.error);
+  const DataState({required this.data, required this.error});
 }
 
 class DataSuccess<T> extends DataState<T> {
-  const DataSuccess(T data) : super(data: data);
+  const DataSuccess(T data) : super(data: data, error: null);
 }
 
 class DataFailure<T> extends DataState<T> {
-  const DataFailure(DioError error) : super(error: error);
+  const DataFailure(DioError error) : super(data:null, error: error);
 }
